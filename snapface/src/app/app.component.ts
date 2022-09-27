d1b7f02b681f47162c400b6ae9dfc2b97c835b9a
Gloria BESAMBA <gloria.besamba@axyus.local.axyus.com>
2022-09-27 16:52:49 +0200

    ngIf - ngFor - ngStyle - ngClass

--- snapface/src/app/app.component.ts
+++ snapface/src/app/app.component.ts
@@ -7,15 +7,32 @@ import { FaceSnap } from './models/face-snap.model';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit{
-  mySnap!: FaceSnap
+  faceSnaps! : FaceSnap[];
 
  ngOnInit() {
-  this.mySnap = new FaceSnap(
-    "Archibald",
-    "Mon meilleur ami depuis tout petit! ",
-    "https://i.ibb.co/cvf34Hb/pexels-cottonbro-5473957.jpg",
-    new Date(),
-    0
-  );  
+  this.faceSnaps = [
+    {
+      title: "Archibald",
+      description: "Mon meilleur ami depuis tout petit! Bald ",
+      imageUrl: "https://i.ibb.co/cvf34Hb/pexels-cottonbro-5473957.jpg",
+      createdDate: new Date(),
+      snaps: 0,
+   },
+  {
+      title: "Archimède",
+      description: "Mon meilleur ami depuis tout petit! Mède",
+      imageUrl: "https://i.ibb.co/cvf34Hb/pexels-cottonbro-5473957.jpg",
+      createdDate: new Date(),
+      snaps: 20,
+      location: "Paris"
+    },
+   {
+      title: "ArchiBolt",
+      description: "Mon meilleur ami depuis tout petit! Bolt",
+      imageUrl: "https://i.ibb.co/cvf34Hb/pexels-cottonbro-5473957.jpg",
+      createdDate: new Date(),
+      snaps: 140,
+    }
+  ]
  }
 }
--- snapface/src/app/face-snap/face-snap.component.ts
+++ snapface/src/app/face-snap/face-snap.component.ts
@@ -10,35 +10,20 @@ import { FaceSnap } from '../models/face-snap.model';
 export class FaceSnapComponent implements OnInit {
   @Input()
   faceSnap!: FaceSnap;
-  
-  title!: string;
-  description!: string;
-  createdDate!: Date;
-  snaps!: number;
-  imageUrl!: string;
   buttonText!: string;
-  snapped!: boolean;
 
   ngOnInit() {
-    //requêtes serveurs pour les données dynamiques
-    this.title = "Archibald";
-    this.description = "Mon meilleur ami depuis tout petit !";
-    this.createdDate = new Date(); //crée à l'instant où l'instance de la classe est générée
-    this.snaps = 6;
-    this.imageUrl = "https://i.ibb.co/cvf34Hb/pexels-cottonbro-5473957.jpg"
-    this.buttonText = "Oh snap";
+    this.buttonText = "Like !";
     
   }
 
   onSnap() {
-
-    if (this.buttonText === "Oh snap") {
+    if (this.buttonText === "Like !") {
       this.faceSnap.snaps++;
-      this.buttonText = "Oops, unSnap!"
+      this.buttonText = "DisLike !"
     } else {
         this.faceSnap.snaps--;
-        this.buttonText = "Oh, Snap!"
-     
+        this.buttonText = "Like !"   
     }
   }
 
