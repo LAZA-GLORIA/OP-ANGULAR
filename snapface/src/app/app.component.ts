417fdb847acc31359dd75a26b6725b0fe3d3c303
Gloria BESAMBA <gloria.besamba@axyus.local.axyus.com>
2022-10-03 14:26:18 +0200

    Operateurs haut niveau mergeMap, concatMap, exhaustMap, switchMap

--- snapface/src/app/app.component.ts
+++ snapface/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { filter, interval, map, Observable, tap } from 'rxjs';
+import { delay, interval, map, mergeMap, of, take, tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,9 +7,37 @@ import { filter, interval, map, Observable, tap } from 'rxjs';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit{
+  redTrainsCalled = 0; 
+  yellowTrainsCalled = 0;
 
-  interval$!: Observable<string>;
  ngOnInit() {
+  interval(500).pipe(
+    take(10),
+    map(value => value % 2 === 0 ? 'rouge' : 'jaune'),
+    tap(color => console.log(`La lumière s'allume en %c${color}`, `color: ${this.translateColor(color)}`)),
+    mergeMap(color => this.getTrainObservable$(color)),
+    tap(train => console.log(`Train %c${train.color} ${train.trainIndex} arrivé !`, `font-weight: bold; color: ${this.translateColor(train.color)}`))
+  ).subscribe();
+}
+
+getTrainObservable$(color: 'rouge' | 'jaune') {
+  const isRedTrain = color === 'rouge';
+  isRedTrain ? this.redTrainsCalled++ : this.yellowTrainsCalled++;
+  const trainIndex = isRedTrain ? this.redTrainsCalled : this.yellowTrainsCalled;
+  console.log(`Train %c${color} ${trainIndex} appelé !`, `text-decoration: underline; color: ${this.translateColor(color)}`);
+  return of({ color, trainIndex }).pipe(
+    delay(isRedTrain ? 5000 : 6000)
+  );
+}
+
+translateColor(color: 'rouge' | 'jaune') {
+  return color === 'rouge' ? 'red' : 'yellow';
+}
+}
+
+/**
+ * interval$!: Observable<string>;
+ *  ngOnInit() {
   this.interval$ = interval(1000).pipe(
     filter(value => value % 3 === 0),
     map(value => value % 2 === 0 ?
@@ -23,4 +51,4 @@ export class AppComponent implements OnInit{
  logger(text: string) {
   console.log(`Log ${text}`);
  }
-}
+ */
